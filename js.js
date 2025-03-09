ead0f0888419323497726f43bf1c414fc4f61779
аааааааааааааа <154531627+mozgotra@users.noreply.github.com>
2025-03-09 21:35:26 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,37 @@
+// let a =3;
+// let b = 2;
+// // alert(a+b);
+// // let c = prompt("xyu");
+// // c = Number(c);
+// function send_hello(){
+//     //let c = prompt("xyu");
+//     // c = Number(c);
+//     // alert(c)
+//     document.write("<h1>Hello</h1>")
+// }
+// // function hcek(){
+// //     a = document.getElementById('first').value
+// //     b = document.getElementById('second').value
+// //     sum = 0;
+    
+// //     while(sum!=1000000000000){
+// //         sum = Number(a)+Number(b) + sum;
+// //         alert(sum)
+// //     }
+// // }
+// function telll(){
+//     if(document.getElementById('tel').value.length==11){
+//         alert("good");
+//     }else{
+//         alert("ne good");
+//     }
+// };
+
+// // alert(c+1);
+// // alert(c-1);
+// // alert(c/1);
+// // alert(c*1);
+// function ddd(){
+//     let myDiv = document.getElementById('div_main_glav');
+//     myDiv.style.backgroundColor = '#1DAD00';
+// }
